c246376c226c33278af7611f026ed81d2a280128
David Miller <dmiller@mtechenterprises.com>
2019-02-19 22:43:59 -0600

    finished with responsive menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-$('.quotes').slick({
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    speed: 800,
-    slidesToShow: 1,
-    adaptiveHeight: true
-});
+// $('.quotes').slick({
+//     dots: true,
+//     infinite: true,
+//     autoplay: true,
+//     autoplaySpeed: 6000,
+//     speed: 800,
+//     slidesToShow: 1,
+//     adaptiveHeight: true
+// });
+
+const slide = ()=> {
+
+    const svg = document.querySelector('svg');
+    const nav = document.querySelector('nav');
+
+    svg.addEventListener('click', ()=> {
+        nav.classList.toggle('show');
+    });
+}
+
+slide();
